f06f888291b8c42686cf56a79aa2a59f414b1f4b
Colten Wilcox <50940139+ColtenreQ@users.noreply.github.com>
2023-06-26 23:09:38 -0500

    Adding files from replit
    Just the basic files

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let animalForm = document.getElementById('animalForm');
+let testSubmit = document.getElementById('testButton');
+
+let animalName;
+let itemName;
+let amountName;
+let listArray = [["Jackrabbit", "Perfect Skin", "3"], ["Boar", "Legendary Skin", "3"]];
+
+animalForm.addEventListener("submit", (animalStuff) => {
+  animalStuff.preventDefault();
+
+  animalName = document.getElementById('animalName').value;
+  itemName = document.getElementById('itemName').value;
+  amountName = document.getElementById('amount').value;
+
+  listArray = [animalName, itemName, amountName];
+
+  let htmlTable = document.getElementById("animalList");
+
+  let tr = document.createElement("tr");
+  tr.className = "tableRow";
+  htmlTable.appendChild(tr);
+  for (i = 0; i < listArray.length; i++) {
+    let td = document.createElement("td");
+    td.innerText = listArray[i];
+    td.className = "items";
+    htmlTable.appendChild(td);
+  }
+
+});
+
+
+
